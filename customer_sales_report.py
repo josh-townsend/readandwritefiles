9d5bef9ebdc712bb37dabba2e662a8ed7ff16e83
Josh Townsend <josh_townsend1@baylor.edu>
2024-06-10 21:08:18 -0500

    fixed sales report

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -1,22 +1,30 @@
 import csv
 
-sales = open('sales.csv','r',newline='')
-sales_file = csv.reader(sales,delimiter=' ')
-outfile = open('salesreport.csv', 'w', newline='')
+customer_totals = {}
 
-outfile.write('Customer','Total')
+sales = open('sales.csv', 'r', newline='')
+sales_file = csv.reader(sales, delimiter=',')
 next(sales_file)
-
+    
 for row in sales_file:
-    id = row[0]
-    odate = row[1]
-    sdate = row[2]
+    id = int(row[0])
     subtotal = float(row[3])
     tax = float(row[4])
-    freight = float(row[5])
-
+    freight = float(row[5])   
+        
     total = subtotal + tax + freight
-    print(id, total)
+        
+    
+    if id in customer_totals:
+        customer_totals[id] += total
+    else:
+        customer_totals[id] = total
 
+outfile = open('salesreport.csv', 'w', newline='')
+outfile.write('Customer , Total\n') 
+
+for id, total in customer_totals.items():
+    outfile.write(f"{id}{'  '}{(total):.2f}\n")
 
 outfile.close()
+
